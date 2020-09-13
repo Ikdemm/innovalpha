aa11839b596e7892cd712deb5f1e378095895efa
Ikdemm <ikdem.benmbarek@aiesec.net>
2020-09-13 22:15:29 +0100

    init server and client

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="page-container">
+        <h1 class="page-header">Enregistrement d’une marque suisse</h1>    
+      </div>
+
+      <div class="form-container">
+        <form>
+
+        
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
