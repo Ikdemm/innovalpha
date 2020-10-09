118adc8ff77c8122fc28bc228b752e082dfa75f8
Ikdemm <ikdem.benmbarek@aiesec.net>
2020-10-09 21:34:15 +0100

    added redux :twisted_rightwards_arrows:

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import axios from 'axios';
-import fileSaver from 'file-saver';
+import React from "react";
+import axios from "axios";
+import fileSaver from "file-saver";
 
-import ApplicantSubForm from './components/ApplicantSubForm';
-import AgentSubForm from './components/AgentSubForm';
-import ContactSubForm from './components/ContactSubForm';
-import BrandSubForm from './components/BrandSubForm';
-import TaxesSubForm from './components/TaxesSubForm';
-import ServicesSubForm from './components/ServicesSubForm';
-import BrandCategroy from './components/BrandCategory';
-import Colors from './components/Colors';
-import Notes from './components/Notes';
-import Appendings from './components/Appendings';
-import Date from './components/Date';
+import ApplicantSubForm from "./components/ApplicantSubForm";
+import AgentSubForm from "./components/AgentSubForm";
+import ContactSubForm from "./components/ContactSubForm";
+import BrandSubForm from "./components/BrandSubForm";
+import TaxesSubForm from "./components/TaxesSubForm";
+import ServicesSubForm from "./components/ServicesSubForm";
+import BrandCategroy from "./components/BrandCategory";
+import Colors from "./components/Colors";
+import Notes from "./components/Notes";
+import Appendings from "./components/Appendings";
+import Date from "./components/Date";
 
-import './App.css';
+import "./App.css";
 
 export default class App extends React.Component {
-
   constructor(props) {
-    super(props)
-    this.state =  {}
+    super(props);
+    this.state = {};
     this.handleChange = this.handleChange.bind(this);
     this.handleCheck = this.handleCheck.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -28,34 +27,39 @@ export default class App extends React.Component {
   }
 
   createAndDownloadPDF() {
-    console.log("Getting the file")
-    axios.post('http://www.localhost:5000/create-proposal', this.state)
-    .then(() => axios({
-      url: 'http://www.localhost:5000/fetch-proposal',
-      method: 'GET',
-      responseType: 'blob'
-    }))
-    .then((response) => {
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', 'file.pdf');
-      document.body.appendChild(link);
-      link.click();
-    });
+    console.log(this.state);
+    console.log("Getting the file");
+    axios
+      .post("http://www.localhost:5000/create-proposal", this.state)
+      .then(() =>
+        axios({
+          url: "http://www.localhost:5000/fetch-proposal",
+          method: "GET",
+          responseType: "blob",
+        })
+      )
+      .then((response) => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "file.pdf");
+        document.body.appendChild(link);
+        link.click();
+      });
   }
 
   handleChange(event) {
     let property = event.target.name;
-    this.setState({[property]: event.target.value}, () => { console.log(this.state[property]) }) 
+    this.setState({ [property]: event.target.value });
   }
 
   handleCheck(event) {
-    console.log(event.target.name)
+    console.log("yo");
+    console.log(event.target.name);
     let property = event.target.name;
-    this.setState({[property]: !this.state[property]}
-      , () => {console.log(this.state)}
-      )
+    this.setState({ [property]: !this.state[property] }, () => {
+      console.log(this.state);
+    });
   }
 
   handleSubmit(event) {
@@ -64,33 +68,39 @@ export default class App extends React.Component {
   }
 
   render() {
-    return <div className="App">
-      <div className="page-container">
-        <h1 className="page-header">Enregistrement d’une marque suisse</h1>    
-      </div>
+    return (
+      <div className="App">
+        <div className="page-container">
+          <h1 className="page-header">Enregistrement d’une marque suisse</h1>
+        </div>
 
-      <div className="form-container">
-        <form onSubmit={this.handleSubmit}>
-        
-        <ApplicantSubForm handleChange={this.handleChange}/>
-        <AgentSubForm handleChange={this.handleChange}/>
-        {/* <ContactSubForm handleChange={this.handleChange}/>
-        <BrandSubForm handleCheck={this.handleCheck} handleChange={this.handleChange}/>
-        <TaxesSubForm handleCheck={this.handleCheck} handleChange={this.handleChange}/>
-        <ServicesSubForm handleChange={this.handleChange}/>
-        <BrandCategroy handleCheck={this.handleCheck} handleChange={this.handleChange}/>
-        <Colors handleChange={this.handleChange}/>
-        <Notes handleChange={this.handleChange}/>
-        <Appendings handleCheck={this.handleCheck} handleChange={this.handleChange}/>
-        <Date handleChange={this.handleChange}/> */}
+        <div className="form-container">
+          <form onSubmit={this.handleSubmit}>
+            <ApplicantSubForm handleChange={this.handleChange} />
+            <AgentSubForm handleChange={this.handleChange} />
+            <ContactSubForm
+              handleChange={this.handleChange}
+              handleCheck={this.handleCheck}
+            />
+            {/* <BrandSubForm handleCheck={this.handleCheck} handleChange={this.handleChange}/>
+    <TaxesSubForm handleCheck={this.handleCheck} handleChange={this.handleChange}/>
+    <ServicesSubForm handleChange={this.handleChange}/>
+    <BrandCategroy handleCheck={this.handleCheck} handleChange={this.handleChange}/>
+    <Colors handleChange={this.handleChange}/>
+    <Notes handleChange={this.handleChange}/>
+    <Appendings handleCheck={this.handleCheck} handleChange={this.handleChange}/>
+    <Date handleChange={this.handleChange}/> */}
 
-        <hr/>
-        
-        <input type="submit" className="submit-button" value="Générer PDF"/>
-        
-        </form>
+            <hr />
+
+            <input
+              type="submit"
+              className="submit-button"
+              value="Générer PDF"
+            />
+          </form>
+        </div>
       </div>
-    </div>
+    );
   }
-  
 }
--- client/src/components/ContactSubForm.js
+++ client/src/components/ContactSubForm.js
@@ -1,64 +1,110 @@
 import React from "react";
 
 export default class ContactSubForm extends React.Component {
-    render() {
-        return(
-            <div>
-                <div className="formheader-container">
-                    <div className="section-formheader-container">
-                        <h3 className="section-header">Personne à contacter</h3>
-                    </div>
-                    <div className="collapse-button-container">
-                        <a>
-                            <h3 className="section-header collapse-button">+</h3>
-                        </a>
-                    </div>    
-                </div>        
-                    
+  constructor(props) {
+    super(props);
+  }
 
-            <div className="subform-container">
-                <div className="form-row">
+  render() {
+    return (
+      <div>
+        <div className="formheader-container">
+          <div className="section-formheader-container">
+            <h3 className="section-header">Personne à contacter</h3>
+          </div>
+          <div className="collapse-button-container">
+            <a>
+              <h3 className="section-header collapse-button">+</h3>
+            </a>
+          </div>
+        </div>
 
-                    {/* <!-- First Name --> */}
-                    <div className="text-input">
-                        <label>Prénom</label>
-                        <input type = "text" className="form-control" id="contactFirstName" name="contactFirstName" onChange={this.props.handleChange} required/>
-                    </div>
-    
-                    {/* <!-- Last Name --> */}
-                    <div className="text-input">
-                        <label>Nom</label>
-                        <input type = "text" className="form-control" id="contactLastName" name="contactLastName" onChange={this.props.handleChange} required/>
-                    </div>
-    
-                    {/* <!-- Social Reason --> */}
-                    <div className="text-input">
-                        <label>Téléphone</label>
-                        <input type = "text" className="form-control" id="contactPhone" name="contactPhone" onChange={this.props.handleChange} required/>
-                    </div>
-
-                    {/* <!-- Application number --> */}
-                    <div className="">
-                        <label>Numéro de référence, dossier</label>
-                        <input type = "text" className="form-control" id="appNumber" name="appNumber" onChange={this.props.handleChange} required/>
-                    </div>
-
-                    {/* <!-- Digital Communication --> */}
-                    <div className="">
-                        <input type="checkbox" className="checkbox-input" id="providedEmail" name="providedEmail"/>
-                        <label>Dans la présente procédure, je souhaite recevoir les écrits de l’IPI par voie électronique à l’adresse suivante*:</label>
-                    </div>
-                    <div className="">
-                        <input type="text" className="form-control" name="email" id="email"/>
-                    </div>
-                    <p className="input-hint">* L’adresse indiquée doit être enregistrée sur une plateforme de messagerie reconnue pour transmettre par voie électronique des documents dans le cadre d’une procédure administrative
-                        fédérale. Pour plus d’informations: www.ipi.ch/communication-electronique.</p>
-                    </div>
-    
-                </div>
+        <div className="subform-container">
+          <div className="form-row">
+            {/* <!-- First Name --> */}
+            <div className="text-input">
+              <label>Prénom</label>
+              <input
+                type="text"
+                className="form-control"
+                id="contactFirstName"
+                name="contactFirstName"
+                onChange={this.props.handleChange}
+                required
+              />
             </div>
-            
-        )
-    }
+
+            {/* <!-- Last Name --> */}
+            <div className="text-input">
+              <label>Nom</label>
+              <input
+                type="text"
+                className="form-control"
+                id="contactLastName"
+                name="contactLastName"
+                onChange={this.props.handleChange}
+                required
+              />
+            </div>
+
+            {/* <!-- Social Reason --> */}
+            <div className="text-input">
+              <label>Téléphone</label>
+              <input
+                type="text"
+                className="form-control"
+                id="contactPhone"
+                name="contactPhone"
+                onChange={this.props.handleChange}
+                required
+              />
+            </div>
+
+            {/* <!-- Application number --> */}
+            <div className="">
+              <label>Numéro de référence, dossier</label>
+              <input
+                type="text"
+                className="form-control"
+                id="appNumber"
+                name="appNumber"
+                onChange={this.props.handleChange}
+                required
+              />
+            </div>
+
+            {/* <!-- Digital Communication --> */}
+            <div className="">
+              <input
+                type="checkbox"
+                className="checkbox-input"
+                id="providedEmail"
+                name="providedEmail"
+                onChange={this.props.handleCheck}
+              />
+              <label>
+                Dans la présente procédure, je souhaite recevoir les écrits de
+                l’IPI par voie électronique à l’adresse suivante*:
+              </label>
+            </div>
+            <div className="">
+              <input
+                type="text"
+                className="form-control"
+                name="contactEmail"
+                id="email"
+                onChange={this.props.handleChange}
+              />
+            </div>
+            <p className="input-hint">
+              * L’adresse indiquée doit être enregistrée sur une plateforme de
+              messagerie reconnue pour transmettre par voie électronique des
+              documents dans le cadre d’une procédure administrative fédérale.
+              Pour plus d’informations: www.ipi.ch/communication-electronique.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-        
