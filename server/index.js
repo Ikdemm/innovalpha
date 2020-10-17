69217ff33ce553150580c1dac6bcf07477b7f48f
Ikdemm <ikdem.benmbarek@aiesec.net>
2020-10-17 08:28:25 +0100

    attempt with multer

--- server/index.js
+++ server/index.js
@@ -8,7 +8,33 @@ const pdfTemplate = require('./documents');
 const wkhtmltopdf = require('wkhtmltopdf');
 wkhtmltopdf.command = "C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe";
 
-const fileUpload = require('express-fileupload');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+
+const fileFilter = (req, file, cb) => {
+  //accept a file
+  if (file.mimetype ===  'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true)
+  }
+  //reject a file
+  console.log("invalid file")
+  cb(null, false)
+}
+
+const upload = multer({storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+})
 
 const app = express();  
 
@@ -17,23 +43,27 @@ const port = process.env.PORT || 5000;
 // Setting up our Middlewares
 // Using Cors middleware to enable CORS with various options
 app.use(cors())
-app.use(fileUpload());
 
 // Using body-parser middelware for parsing req.body
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use('/uploads', express.static("uploads"))
+
 // POST - PDF Generation and fetching Data
-app.post('/create-proposal', (req, res) => {
+app.post('/create-proposal', upload.single('brand'), (req, res) => {
 
     console.log("Creating File ..")
 
-    if (!req.files) {
-      return res.status(500).send({ msg: "file is not found" })
-    }
+    // if (!req.files) {
+    //   return res.status(500).send({ msg: "file is not found" })
+    // }
 
+    console.log(req.file);
+
+    console.log(req.body);
   
-    const brand = req.files.brand;
+    // const brand = req.files.brand;
 
     // mv() method places the file inside public directory
     brand.mv(`${path.join(__dirname, '..', 'public/brand.jpg')}`, function (err) {
@@ -49,12 +79,7 @@ app.post('/create-proposal', (req, res) => {
 
   });
 
-    
-    // wkhtmltopdf(pdfTemplate(data), {
-    //     output: `${__dirname}/proposal.pdf`,
-    //     pageSize: 'letter'
-    // });
-// });
+});
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
@@ -64,4 +89,4 @@ app.get('/fetch-proposal', (req, res) => {
     res.download(`${__dirname}/out/proposal.pdf`)
 })
 
-app.listen(port, () => console.log(`Listening on port ${port} ...`))
+app.listen(port, () => console.log(`Listening on port ${port} ...`));
