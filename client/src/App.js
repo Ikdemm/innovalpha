6c9e6f602d5633b7c6a1c37791502589d87f488c
Ikdemm <ikdem.benmbarek@aiesec.net>
2020-11-29 10:55:25 +0100

    created configure-store and used in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { Container } from './container';
-
-let store = createStore(languageReducer)
-
-const languageReducer = function (state = 'fr', action) {
-  switch (action.type) {
-    case 'fr':
-      return state = 'fr';
-    case 'de': 
-      return state = 'de';
-    case 'it':
-      return state = 'it';
-    default:
-      return state;
-  }
-}
+import { store } from './configure-store';
 
 const App = () => (
   <Provider store={store}>
