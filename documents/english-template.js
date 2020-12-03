5bc28423515bcebd23b13e1c00435c12e5bde604
Ikdemm <ikdem.benmbarek@aiesec.net>
2020-12-03 22:49:12 +0100

    finished with the english-template file

--- documents/english-template.js
+++ documents/english-template.js
@@ -67,23 +67,23 @@ module.exports = (data) => {
                     
                     <div class="content-container">
 
-                        <h1 class="title">Enregistrement d'une marque suisse</h1>
+                        <h1 class="title">Registration of a Swiss trademark</h1>
                         <p class="paragraph-text">
-                            <b>&bull;</b> La brochure «Dépôt d’une marque» contient des renseignements sur les formalités de dépôt et la manière de remplir ce formulaire
+                            <b>&bull;</b> The brochure "Filing a trademark" contains information on the filing formalities and how to complete this form
                         </p>
                         <p class="paragraph-text">
-                            <b>&bull; Mise en garde:</b> Au cours de la procédure d’enregistrement, nous n’examinons pas d’éventuelles atteintes à des marques ou
-                            raisons de commerce déjà enregistrées. C’est pourquoi nous vous conseillons vivement de faire faire une recherche professionnelle
-                            d’antériorités de marques similaires et identiques ainsi que de sociétés. Informations complémentaires sur www.ipi.ch. Nous vous
-                            recommandons en outre de demander l’appui d’un conseil en marques (www.ipi.ch/conseils-marques).
+                            <b>&bull; Caution:</b> During the registration process, we do not examine any possible trademark infringements or
+                            business reasons already registered. This is why we strongly advise you to have a professional research done
+                            prior art of similar and identical brands as well as companies. Further information on www.ipi.ch. We you
+                            furthermore recommend seeking the support of a trademark advisor (www.ipi.ch/conseils-marques).
                         </p>
                         <p class="paragraph-text">
-                            <b>&bull; Dépôt électronique d’une marque par Internet: https://e-trademark.ige.ch</b>
+                            <b>&bull; Digital filing of a trademark via the Internet: https://e-trademark.ige.ch</b>
                         </p>
                         
                         <div class="form-container">
-                            <h3 class="section-title">1  Déposant/e</h3>
-                            <p class="paragraph-text">Prénom, nom ou raison sociale, adresse, code postal, localité</p>
+                            <h3 class="section-title">1  Depositor/h3>
+                            <p class="paragraph-text">First name, surname, company name, address, postal code, town</p>
                             <p class="data-text">
                             ${data.applicantFirstName},
                             ${data.applicantLastName}, 
@@ -95,8 +95,8 @@ module.exports = (data) => {
                     
                     
                             <!------------ Mandataire --------------->
-                            <h3 class="section-title">2  Mandataire</h3>
-                            <p class="paragraph-text">Prénom, nom ou raison sociale, adresse, code postal, localité</p>
+                            <h3 class="section-title">2  Representative</h3>
+                            <p class="paragraph-text">First name, surname, company name, address, postal code, town</p>
                             <p class="data-text">
                             ${data.agentFirstName},
                             ${data.agentLastName}, 
@@ -107,44 +107,44 @@ module.exports = (data) => {
                             </p>
                     
                             <!------------ Contact --------------->
-                            <h3 class="section-title">3a  Personne à contacter</h3>
-                            <p class="paragraph-text">Prénom, nom, téléphone</p>
+                            <h3 class="section-title">3a  Contact Person</h3>
+                            <p class="paragraph-text">First name, surname, phone number</p>
                             <p class="data-text">
                             ${data.contactFirstName},
                             ${data.contactLastName}, 
                             ${data.contactPhone}
                             </p>
         
-                            <h3 class="section-title">3b  Numéro de référence, dossier</h3>
+                            <h3 class="section-title">3b  Reference number, file</h3>
                             <p class="data-text">${data.appNumber}</p>
         
-                            <h3 class="section-title">3c  Communication électronique</h3>
+                            <h3 class="section-title">3c  Digital communication</h3>
                             <br>
                             <label for="communication" class="paragraph-text">
-                                Dans la présente procédure, je souhaite recevoir les écrits de l’IPI par voie électronique
-                                à l’adresse suivante*:
+                                In this procedure, I wish to receive the writings of the IPI electronically
+                                to the following address*:
                             </label>
                             <p class="data-text">${data.contactEmail}</p>
-                            <p class="paragraph-text">* L’adresse indiquée doit être enregistrée sur une plateforme de messagerie reconnue pour transmettre par voie électronique des documents dans le cadre d’une procédure administrative
-                                fédérale. Pour plus d’informations: www.ipi.ch/communication-electronique.
+                            <p class="paragraph-text">* The address indicated must be registered on a recognized messaging platform for the electronic transmission of documents as part of an administrative procedure
+                                federal. For more information: www.ipi.ch/communication-electronique.
                             </p>
 
 
                             <!------------ Marque --------------->
-                            <h3 class="section-title">4  Marque</h3>
+                            <h3 class="section-title">4  Brand</h3>
                             <p class="paragraph-text">
-                                S’il s’agit d’une marque présentant
-                                une police de caractère particulière
-                                ou faisant intervenir un graphisme
-                                particulier, veuillez déposer une reproduction en noir et blanc (à insérer ou coller ci-contre). Les zones
-                                grises doivent apparaître distinctement. Pour les marques en couleur(s)
-                                (voir ch. 9 couleurs revendiquées),
-                                veuillez déposer une reproduction en
-                                couleur(s). 
+                                If it is a brand with
+                                a particular font
+                                or involving graphics
+                                In particular, please file a black and white reproduction (to be inserted or pasted opposite). Areas
+                                gray should appear distinctly. For brands in color (s)
+                                (see ch. 9 colors claimed),
+                                please file a reproduction in
+                                colors).
                             </p>
                             <p class="paragraph-text">
                                 <b>
-                                    Dimensions maximales de la
+                                    Maximum dimensions of the
                                     reproduction: 80 × 80 mm.   
                                 </b>
                             </p>
@@ -153,42 +153,42 @@ module.exports = (data) => {
                             <!------------ Taxes --------------->
                             <h3 class="section-title">5  Taxes</h3>
 
-                            <p class="paragraph-text">Taxes de dépôt</p>
-                            <p class="data-text">${data.depositTax} à nous facturer</p>
-                            <p class="data-text">${data.depositSum} à débiter de notre compte courant n° ${data.taxDepositAccount}</p>
+                            <p class="paragraph-text">Deposit taxes</p>
+                            <p class="data-text">${data.depositTax} to bill us</p>
+                            <p class="data-text">${data.depositSum} to be debited from our current account no. ${data.taxDepositAccount}</p>
                             
-                            <p class="paragraph-text">Surtaxes pour classe suplémentaire</p>
-                            <p class="data-text">${data.additionalSum} à nous facturer</p>
-                            <p class="data-text">${data.additionalSumTax} à débiter de notre compte courant n° ${data.additionalTaxDepositAccount}</p>
+                            <p class="paragraph-text">Surcharges for additional class</p>
+                            <p class="data-text">${data.additionalSum} to bill us</p>
+                            <p class="data-text">${data.additionalSumTax} to be debited from our current account no. ${data.additionalTaxDepositAccount}</p>
                         
                             <!------------ Prducts/services --------------->
-                            <h3 class="section-title">6  Produits</h3>
+                            <h3 class="section-title">6  Products</h3>
                             <p class="paragraph-text">${data.products}</p>
                         
                             <!------------ Brand Category --------------->
-                            <h3 class="section-title">7  Catégorie de Marque</h3>
+                            <h3 class="section-title">7  Brand Category</h3>
                             <p class="paragraph-text">${data.brandCategory}</p>
 
                             <!------------ Claim --------------->
-                            <h3 class="section-title">8  Revendication de priorité</h3>
-                            <p class="paragraph-text">Selon la convention de l’Union de Paris</p>
-                            <p class="paragraph-text">Pays: <span class="data-text">${data.country}</span></p>
-                            <p class="paragraph-text">Date de premier dépôt: <span class="data-text">${data.depositDate}</span></p>
+                            <h3 class="section-title">8  Priority claim</h3>
+                            <p class="paragraph-text">According to the Paris Union convention</p>
+                            <p class="paragraph-text">Country: <span class="data-text">${data.country}</span></p>
+                            <p class="paragraph-text">Date of first deposit: <span class="data-text">${data.depositDate}</span></p>
 
                             <!------------ Colors --------------->
-                            <h3 class="section-title">9  Couleur(s) revendiquée(s)</h3>
+                            <h3 class="section-title">9  Color(s) claimed</h3>
                             <p class="paragraph-text">${data.colors}</p>
 
                             <!------------ Notes --------------->
-                            <h3 class="section-title">10  Remarques</h3>
+                            <h3 class="section-title">10  Notes</h3>
                             <p class="paragraph-text">${data.notes}</p>
 
                             <!------------ Appendings --------------->
-                            <h3 class="section-title">11  Annexes</h3>
-                            <p class="paragraph-text">Annexe 1: ${data.appendingFile1}</p>
-                            <p class="paragraph-text">Annexe 2: ${data.appendingFile2}</p>
-                            <p class="paragraph-text">Annexe 3: ${data.appendingFile3}</p>
-                            <p class="paragraph-text">Annexe 4: ${data.appendingFile4}</p>
+                            <h3 class="section-title">11  Appendings</h3>
+                            <p class="paragraph-text">Appending 1: ${data.appendingFile1}</p>
+                            <p class="paragraph-text">Appending 2: ${data.appendingFile2}</p>
+                            <p class="paragraph-text">Appending 3: ${data.appendingFile3}</p>
+                            <p class="paragraph-text">Appending 4: ${data.appendingFile4}</p>
 
                         </div>
                     </div>
